cc385639f52af4099599f6e59e50c52db7f34e30
Animesh Roy <animeshroy@live.com>
2020-12-09 17:09:43 +0530

    Update dbconf.php

--- dbconf.php
+++ dbconf.php
@@ -1,5 +1,5 @@
 <?php
-$con = new mysqli("127.0.0.1", "dscidba", "Passw0rd!20", "userdb");
+$con = new mysqli("127.0.0.1", "dbusername", "dbpassword", "databasename");
 if ($con -> connect_error){
     die("Database Not Configured Properly");
 }
